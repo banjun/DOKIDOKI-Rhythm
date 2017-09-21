7381359dfd484fc1785000a6e63eaf734321178a
banjun <banjun@gmail.com>
2017-09-22 00:09:55 +0900

    fetch the first 100 heartrate records

--- DOKIDOKI Rhythm/ViewController.swift
+++ DOKIDOKI Rhythm/ViewController.swift
@@ -1,4 +1,7 @@
 import UIKit
+import HealthKit
+
+private let store = HKHealthStore()
 
 class ViewController: UIViewController {
     init() {
@@ -6,5 +9,31 @@ class ViewController: UIViewController {
     }
 
     required init?(coder aDecoder: NSCoder) {fatalError()}
-}
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let typesToRead: [HKObjectType] = [.quantityType(forIdentifier: .heartRate)].flatMap {$0}
+
+        store.requestAuthorization(toShare: nil, read: Set(typesToRead)) { granted, error in
+            guard granted, error == nil else { NSLog("%@", "granted = \(granted), error = \(String(describing: error))"); return }
+            self.healthStoreDidSetup()
+        }
+    }
+
+    private func healthStoreDidSetup() {
+        store.execute(HKAnchoredObjectQuery(type: HKSampleType.quantityType(forIdentifier: .heartRate)!, predicate: nil, anchor: nil, limit: HKObjectQueryNoLimit) { query, samples, deletedObjects, anchor, error in
+            NSLog("%@", "\(samples?.count ?? 0) samples found")
+            guard let qSamples = samples as? [HKQuantitySample], qSamples.count > 0 else { return }
+
+            let head = qSamples.prefix(upTo: 100)
+            head.forEach { s in
+                let date = s.startDate
+                let bpm = s.quantity.doubleValue(for: HKUnit(from: "count/min"))
+                let sourceRevision = s.sourceRevision
+                let device = s.device
+                print("\(date)\t\(bpm)\t\(sourceRevision.source.name) \(sourceRevision.version ?? "_") on \(device?.name ?? "_")")
+            }
+        })
+    }
+}
