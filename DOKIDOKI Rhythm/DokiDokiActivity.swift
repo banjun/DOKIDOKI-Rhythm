c8df0e044e461f6c37f09ca9b9eab7ee28c2a1c6
banjun <banjun@gmail.com>
2017-12-16 14:29:51 +0900

    fix build

--- DOKIDOKI Rhythm/DokiDokiActivity.swift
+++ DOKIDOKI Rhythm/DokiDokiActivity.swift
@@ -10,7 +10,7 @@ struct DokiDokiActivity: Equatable, Codable {
         var heartrate: Int
     }
 
-    static func == (lhs: Self, rhs: Self) -> Bool {return lhs.start == rhs.start}
+    static func == (lhs: DokiDokiActivity, rhs: DokiDokiActivity) -> Bool {return lhs.start == rhs.start}
 }
 
 let fakeActivities: [DokiDokiActivity] = [
